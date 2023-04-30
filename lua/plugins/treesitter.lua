9117c31c3f923c3e36af8aa7d8490746f4c551e1
Roshan Mishra <roshanmishrarinem@gmail.com>
2023-04-30 18:37:58 +0530

    feat: add lazyvim starter config for neovim

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,26 @@
+return {
+  -- add more treesitter parsers
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+        "pug",
+        "scss",
+      },
+    },
+  }
+}
